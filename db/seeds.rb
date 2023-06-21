1282e970f1d13b080452278f00c1a6bf16b72c39
Kaja Witek <kaja.wasiolek@gmail.com>
2023-06-21 11:08:05 +0200

    feat: add creating users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-puts 'Creating users'
+Rails.logger.debug 'Creating users'
 (1..5).each do |_i|
   User.create!(email: Faker::Internet.email, password: 'password')
 end
