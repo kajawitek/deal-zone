3e027c8eca45f5aa3e2f28d5b7bd307ee96d6c6d
Kaja Witek <kaja.wasiolek@gmail.com>
2023-06-26 21:07:41 +0200

    test: correct user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,6 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  it { is_expected.to validate_presence_of(:username) }
-  it { is_expected.to validate_uniqueness_of(:username) }
+  it 'validates the presence of a username' do
+    user = build(:user, username: nil)
+
+    expect(user).not_to be_valid
+    expect(user.errors.messages[:username]).to include("can't be blank")
+
+    user.username = 'username'
+    expect(user).to be_valid
+  end
+
+  it 'validates the uniqueness of a username' do
+    user = create(:user)
+
+    expect(user).to be_valid
+
+    user2 = build(:user, username: user.username)
+
+    expect(user2).not_to be_valid
+    expect(user2.errors.messages[:username]).to include('has already been taken')
+  end
 end
