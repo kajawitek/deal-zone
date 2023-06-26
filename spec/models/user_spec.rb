c7b1a6648e01cf4df0dd9328dad2f218c70b8359
Kaja Witek <kaja.wasiolek@gmail.com>
2023-06-26 20:19:36 +0200

    feat: add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:username) }
+  it { is_expected.to validate_uniqueness_of(:username) }
 end
