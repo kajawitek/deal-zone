645d14cb64953d1192be5a2b94e1ce482f976b8b
Kaja Witek <kaja.wasiolek@gmail.com>
2023-06-26 20:14:09 +0200

    test: update users factory and user_spec

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe 'Users' do
     visit root_path
     click_link 'Sign up'
 
+    fill_in 'Username', with: user.username
     fill_in 'Email', with: user.email
     fill_in 'Password', with: user.password
     fill_in 'Password confirmation', with: user.password
