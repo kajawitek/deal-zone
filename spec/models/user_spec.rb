bad994e0133160ca5b2bed9e8511b959886523b7
Kaja Witek <kaja.wasiolek@gmail.com>
2023-06-26 21:16:35 +0200

    test: refactor user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,22 +5,12 @@ require 'rails_helper'
 RSpec.describe User do
   it 'validates the presence of a username' do
     user = build(:user, username: nil)
-
     expect(user).not_to be_valid
-    expect(user.errors.messages[:username]).to include("can't be blank")
-
-    user.username = 'username'
-    expect(user).to be_valid
   end
 
   it 'validates the uniqueness of a username' do
     user = create(:user)
-
-    expect(user).to be_valid
-
-    user2 = build(:user, username: user.username)
-
-    expect(user2).not_to be_valid
-    expect(user2.errors.messages[:username]).to include('has already been taken')
+    not_valid_user = build(:user, username: user.username)
+    expect(not_valid_user).not_to be_valid
   end
 end
