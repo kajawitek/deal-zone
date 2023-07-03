8cfa737dbdc14034c52978881ab7203c5ef19fa7
Kaja Witek <kaja.wasiolek@gmail.com>
2023-07-03 18:06:57 +0200

    feat: add product model

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+
+  resources :products, only: %i[index show create update destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_26_172650) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_03_155443) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.decimal "price", null: false
+    t.integer "quantity", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
