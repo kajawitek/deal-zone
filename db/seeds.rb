e08108b07c842fb38b3c4dc3b75b2596dc239185
Kaja Witek <kaja.wasiolek@gmail.com>
2023-07-04 20:54:33 +0200

    refactor: seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 Rails.logger.debug 'Creating users'
-3.times do |_u|
+3.times do
   user = FactoryBot.create(:user)
   puts "# Creating user #{user.username}"
 end
 
-10.times do |_p|
+10.times do
   product = FactoryBot.create(:product)
   puts "# Creating product #{product.name}"
 end
