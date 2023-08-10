e8c69acfa06cf5b08f25381e309de2f6ad5e4b8a
Kaja Witek <kaja.wasiolek@gmail.com>
2023-08-10 12:41:58 +0200

    refactor: remove unnecessary methods from product_policy

--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -1,27 +1,11 @@
 # frozen_string_literal: true
 
-class ProductPolicy
+class ProductPolicy < ApplicationPolicy
   attr_reader :user, :product
 
   def initialize(user, product)
     @user = user
     @product = product
-  end
-
-  def index?
-    false
-  end
-
-  def show?
-    false
-  end
-
-  def create?
-    false
-  end
-
-  def new?
-    create?
   end
 
   def update?
@@ -34,20 +18,5 @@ class ProductPolicy
 
   def destroy?
     update?
-  end
-
-  class Scope
-    def initialize(user, scope)
-      @user = user
-      @scope = scope
-    end
-
-    def resolve
-      raise NotImplementedError, "You must define #resolve in #{self.class}"
-    end
-
-    private
-
-    attr_reader :user, :scope
   end
 end
