bf7ded9f644380321fb09bf38c76bd82756cd19d
Kaja Witek <kaja.wasiolek@gmail.com>
2023-08-23 09:33:57 +0200

    refactor: remove buyer references from products

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,6 @@
 
 class Product < ApplicationRecord
   belongs_to :user
-  belongs_to :buyer, class_name: 'User', optional: true
 
   validates :name, :description, :price, :quantity, presence: true
 end
